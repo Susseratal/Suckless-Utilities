cff1d2948bf2e05082f1d674f616c0d1992c7d17
Iain <jtfjyrx@gmail.com>
2021-06-22 14:37:30 +0100

    updated colour scheme to match st

--- .dwm/config.h
+++ .dwm/config.h
@@ -9,15 +9,21 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+static const char col_white[]       = "#FFFFFF";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan1[]       = "#005577";
+static const char col_cyan2[]       = "#00ff9f";
+static const char col_black[]       = "#000000";
+static const char col_pink1[]       = "#ff33ff";
+static const char col_green1[]      = "#008000";
+static const char col_green2[]      = "#00FF00";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_green2, col_black, col_gray2 },
+	[SchemeSel]  = { col_green2, col_black, col_green1},
 };
 
 /* tagging */
@@ -58,7 +64,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_green2, "-sb", col_green1, "-sf", col_black, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browser[] = { "opera", NULL };
 static const char *discord[] = { "discord", NULL };
